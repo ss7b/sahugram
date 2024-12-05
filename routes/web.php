5bdf6b510c493e7f5d4027c97e1b06688e8ed6e8
Suahil <babks93@gmial.com>
2024-12-05 04:02:21 +0300

    Build post page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Post;
@@ -34,3 +35,7 @@ require __DIR__.'/auth.php';
 
 Route::get('/p/create', [PostController::class, 'create'])->name('create_post')->middleware('auth');
 Route::post('/p/create', [PostController::class, 'store'])->name('store_post')->middleware('auth');
+Route::get('/p/{post:slug}', [PostController::class, 'show'])->name('show_post')->middleware('auth');
+
+Route::post('/p/{post:slug}/comment', [CommentController::class, 'store'])->name('store_comment')->middleware('auth');
+
